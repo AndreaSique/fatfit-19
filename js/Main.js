34ac33408dd2b2e14b3195d325752d2bcb8d706c
andrea <apsique@scl.edu.gt>
2021-10-10 09:25:44 -0600

    Se completo la aplicación al 100%

--- js/Main.js
+++ js/Main.js
@@ -17,17 +17,21 @@ function calcularIMC(elEvento){
     if(formula < 18.5){
         resultadoRespuesta.innerHTML =  '<img class="resultado-respuesta-img" src="img/imgPeso/bajopeso.png">'+
                                         '<h3 class="resultado-imc">Bajo peso</h3>'
+        botonFinalizar.innerHTML = '<a href="pesobajo.html">Continuar</a>'
 
     }else if(formula > 18.5 & formula < 24.9){
         resultadoRespuesta.innerHTML =  '<img class="resultado-respuesta-img" src="img/imgPeso/pesonormal.png">'+
                                         '<h3 class="resultado-imc">Peso normal</h3>'
+        botonFinalizar.innerHTML = '<a href="pesonormal.html">Continuar</a>'
 
     }else if(formula > 25 & formula < 29.9){
         resultadoRespuesta.innerHTML =  '<img class="resultado-respuesta-img" src="img/imgPeso/sobrepeso.png">'+
                                         '<h3 class="resultado-imc">Sobrepeso</h3>'
+        botonFinalizar.innerHTML = '<a href="sobrepeso.html">Continuar</a>'
     }else if(formula >= 30){
         resultadoRespuesta.innerHTML =  '<img class="resultado-respuesta-img" src="img/imgPeso/obesidad.png">'+
                                         '<h3 class="resultado-imc">Obesidad</h3>'
+        botonFinalizar.innerHTML = '<a href="obesidad.html">Continuar</a>'
     }
 }
 var boton = document.querySelector("#botonListo");
